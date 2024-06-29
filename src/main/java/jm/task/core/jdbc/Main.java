519f2911d9eecfb0849bc01242b7b8d7442783d3
dmitriyTASHKINOV <tashkinov.mitiaaaa@gmail.com>
2024-06-29 12:44:25 +0300

    prij

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Dmitry","Tashkinov",(byte) 26);
+        userService.saveUser("Sofi","Tashkinova",(byte) 24);
+        userService.saveUser("Elena","Tashkinova",(byte) 47);
+        userService.saveUser("Alexsandr","Tashkinov",(byte) 16);
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DB_DRIVER ="com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/mydbtest";
+    private static final String DB_USERNAME = "roottyt";
+    private static final String DB_PASSWORD = "roottyt";
+     public static Connection getConnection(){
+         Connection connection = null;
+         try {
+             Class.forName(DB_DRIVER);
+             connection = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+             System.out.println("vse ok");
+         } catch (ClassNotFoundException | SQLException e) {
+             e.printStackTrace();
+             System.out.println("vse not ok");
+         }
+         return connection;
+     }
 }
